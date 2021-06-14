85a80de70688e78926ec130231c5c28fbafc6827
Bob Nassar <nassarbob@outlook.com>
2021-06-14 16:58:35 +0300

    Added input box for the rgba

--- src/app/color-generator/color-generator.page.ts
+++ src/app/color-generator/color-generator.page.ts
@@ -8,9 +8,9 @@ import { ToastController } from '@ionic/angular';
 })
 export class ColorGeneratorPage implements OnInit {
   folder: string;
-  r = 0;
-  g = 0;
-  b = 0;
+  r = 127;
+  g = 127;
+  b = 127;
   a = 1;
   icon = 'chevron-down';
   isExpended = true;
@@ -30,24 +30,76 @@ export class ColorGeneratorPage implements OnInit {
   ngOnInit() {
   }
 
-  expandCollapseContainer(){
+  expandCollapseContainer() {
     const footer = document.querySelector('ion-footer');
+    const headers = document.querySelectorAll('ion-header');
 
-    if(this.isExpended === true){
+    if (this.isExpended === true) {
       this.isExpended = false;
       this.icon = 'chevron-up';
 
       footer.style.height = '0';
-    }else{
+
+      headers.forEach(header => {
+        header.style.top = '-200px';
+      });
+
+    } else {
       this.isExpended = true;
       this.icon = 'chevron-down';
       footer.style.height = '178px';
+
+      headers.forEach(header => {
+        header.style.top = '0';
+      });
     }
   }
 
   async copyToClipboard(): Promise<void> {
-    await navigator.clipboard.writeText(`rgba(${this.r},${this.g},${this.b}, ${this.a})`).then(()=>{
+    await navigator.clipboard.writeText(`rgba(${this.r},${this.g},${this.b}, ${this.a})`).then(() => {
       this.presentToast();
     });
+  }
+
+  checkNumberLimit(ev) {
+
+    switch (ev.target.name) {
+      case 'r':
+        if (Number(ev.target.value) > 255) {
+          this.r = 255;
+          //ev.target.value = '255';
+        } else if (Number(ev.target.value) < 0) {
+          ev.target.value = 0;
+        }
+        break;
+      case 'g':
+        if (Number(ev.target.value) > 255) {
+          this.g = 255;
+          ev.target.value = 255;
+        } else if (Number(ev.target.value) < 0) {
+          ev.target.value = 0;
+        }
+        break;
+      case 'b':
+        if (Number(ev.target.value) > 255) {
+          this.b = 255;
+          ev.target.value = 255;
+        } else if (Number(ev.target.value) < 0) {
+          ev.target.value = 0;
+        }
+        break;
+      case 'a':
+        if (Number(ev.target.value) > 255) {
+          this.a = 255;
+          ev.target.value = 1;
+        } else if (Number(ev.target.value) < 0) {
+          ev.target.value = 0;
+        }
+        break;
+    }
+  }
+
+  setValue(element: HTMLInputElement, colorVar){
+
   }
 }
